cba4065bd3d72f003be2b279a7f2b516feb9adc0
José Beltre <552006jose@gmail.com>
2025-07-24 01:53:57 -0400

    Improved img for project presentation and delete text data for project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,36 +8,15 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    name: 'Colmado César',
+    name: 'Colmado Cesar',
     projectType: 'App web',
     description: 'Aplicación web diseñada para la gestión de ventas y registro de entradas y salidas de dinero en un colmado dominicano. Incluye una interfaz simple, funcional y adaptada al cliente.',
     goal: 'Facilitar el control financiero diario del colmado, permitiendo registrar ingresos, egresos y generar reportes mensuales.',
     images: [
-      'colmado-cesar/1.jpg',
-      'colmado-cesar/2.jpg',
-      'colmado-cesar/3.jpg'
-    ]
-  },
-  // {
-  //   name: 'JosefoDev Portfolio',
-  //   projectType: 'Sitio web personal',
-  //   description: 'Landing page profesional donde presento mis servicios como desarrollador web. Diseño atractivo con scroll, animaciones, y sección de contacto.',
-  //   goal: 'Consolidar mi marca personal y captar clientes potenciales a través de una presentación clara de mis servicios y trabajos.',
-  //   images: [
-  //     'portfolio/1.jpg',
-  //     'portfolio/2.jpg',
-  //     'portfolio/3.jpg'
-  //   ]
-  // },
-  {
-    name: 'Concesionaria de Autos',
-    projectType: 'Landing + Inventario + Dashboard',
-    description: 'Landing page diseñada para una concesionaria de autos con un buscador de inventario público e intuitivo. La página incluye filtros, sección de contacto y presentación de los vehículos. También posee un panel administrativo para gestionar autos, categorías y datos del sitio.',
-    goal: 'Ofrecer una plataforma moderna para que los clientes puedan explorar el inventario disponible y que el concesionario pueda administrarlo fácilmente desde un panel seguro.',
-    images: [
-      'colmado-cesar/1.jpg',
-      'colmado-cesar/2.jpg',
-      'colmado-cesar/3.jpg'
+      'colmado-cesar/1.webp',
+      'colmado-cesar/2.webp',
+      'colmado-cesar/3.webp',
+      'colmado-cesar/4.webp'
     ]
   }
 ]
